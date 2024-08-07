edfd6983013aa0dd5c55eaf92ba5ff9bc9a72e0d
Peekaey <dallasjt31@gmail.com>
2024-08-07 20:02:28 +1000

    Initialise Secret Reader and Repo

--- Program.cs
+++ Program.cs
@@ -1,9 +1,52 @@
-﻿namespace Malcolm_Turnbull_Bot;
+﻿using Discord;
+using Discord.WebSocket;
+using Microsoft.Extensions.Configuration;
 
-class Program
+
+namespace Malcolm_Turnbull_Bot
 {
-    static void Main(string[] args)
+    public class Program
     {
-        Console.WriteLine("Hello, World!");
+        private static DiscordSocketClient _client;
+        public static async Task Main()
+        {
+            string starting = "Starting Bot...";
+            Console.WriteLine(starting);
+
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("secrets.json");
+
+            IConfiguration configuration = builder.Build();
+
+            _client = new DiscordSocketClient();
+
+            _client.Log += Log;
+            
+            string discordToken = configuration["DISCORD_TOKEN"];
+
+            //  You can assign your bot token to a string, and pass that in to connect.
+            //  This is, however, insecure, particularly if you plan to have your code hosted in a public repository.
+
+            // Some alternative options would be to keep your token in an Environment Variable or a standalone file.
+            // var token = Environment.GetEnvironmentVariable("NameOfYourEnvironmentVariable");
+            // var token = File.ReadAllText("token.txt");
+            // var token = JsonConvert.DeserializeObject<AConfigurationClass>(File.ReadAllText("config.json")).Token;
+
+            await _client.LoginAsync(TokenType.Bot, discordToken);
+            await _client.StartAsync();
+
+            // Block this task until the program is closed.
+            await Task.Delay(-1);
+            
+        }
+        
+        private static Task Log(LogMessage msg)
+        {
+            Console.WriteLine(msg.ToString());
+            return Task.CompletedTask;
+        }
+        
+        
     }
 }
--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Malcolm-Turnbull-Bot
+
+Discord Bot that Posts the same photo of Malcolm Turnbull Daily
+
+secrets.json file currently needs to be stored in bin/Debug/net6.0/secrets.json as
+
+{
+    "DISCORD_TOKEN": "example token"
+}
